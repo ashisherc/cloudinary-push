0dfcdff32a0808aeb5f2e4f30b9ba7cf274d7221
Ashish Prajapati <ashish.mscs@gmail.com>
2017-10-25 17:53:13 +0530

    commad to set creds

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 const request = require('request');
 const fs = require('fs');
 const program = require('commander');
+
+const db = require('./db');
+
+
+program
+    .command('config')
+    .option('-p, --preset <preset>', 'Set preset name with config option.')
+    .option('-c, --cloudname <cloudname>', 'Set cloud name with config option.')
+    .action(function (command) {
+        if(!(command.cloudname && command.preset)){
+            console.log("Provide preset name with -p option and cloud name with -c.");
+            return;
+        }
+        var config = {
+            cloudName: command.cloudname,
+            preset: command.preset
+        }
+        console.log("Credentials are set.");
+        db.storeConfig(config);
+    })
+
+program.parse(process.argv);
