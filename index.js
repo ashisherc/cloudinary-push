9d5d63bfe0c86d7c229e2908511196b8c6a9a9f5
Ashish Prajapati <ashish.mscs@gmail.com>
2017-10-25 18:44:56 +0530

    added progress bar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 const program = require('commander');
+const ProgressBar = require('progress');
 
 const db = require('./db');
 
@@ -11,7 +12,7 @@ program
     .option('-p, --preset <preset>', 'Set preset name with config option.')
     .option('-c, --cloudname <cloudname>', 'Set cloud name with config option.')
     .action(function (command) {
-        if(!(command.cloudname && command.preset)){
+        if (!(command.cloudname && command.preset)) {
             console.log("Provide preset name with -p option and cloud name with -c.");
             return;
         }
@@ -25,7 +26,7 @@ program
 
 program
     .command('reset')
-    .action(function(command){
+    .action(function (command) {
         db.resetConfig();
         console.log("Credentials are removed.");
     })
@@ -41,10 +42,22 @@ program
             console.log('Please set cloud name and preset with config command.');
             return;
         }
-        var formData = {
+        const formData = {
             upload_preset: preset,
             file: fs.createReadStream(file)
         }
+        const fileSize = fs.statSync(file).size;
+        const fileStream = formData.file;
+        const barOpts = {
+            width: 50,
+            total: fileSize,
+            clear: true
+        };
+        const bar = new ProgressBar(' uploading [:bar] :percent :etas', barOpts);
+        fileStream.on('data', function (chunk) {
+            bar.tick(chunk.length);
+        });
+
         const url = "https://api.cloudinary.com/v1_1/" + cloudName + "/image/upload";
         request.post({ url: url, formData: formData }, function optionalCallback(err, httpResponse, body) {
             if (err) {
